ebca9f273dd66be060aa8cafc124d0b7a4ca8f7d
Patrick Perey <the4dpatrick@yahoo.com>
2016-11-06 11:37:11 -0800

    PortfolioList

--- src/components/PortfolioList/index.js
+++ src/components/PortfolioList/index.js
@@ -1,14 +1,39 @@
 import React, { PropTypes as P } from 'react';
+import './index.css';
+import { Link } from 'react-router';
+import { moneyFormat } from '../../utils';
 
 const PortfolioList = props => {
   const { portfolio } = props;
   return (
-    <div>
+    <div className="portfolio-list">
+      <div className="row header">
+        <div className="cell">
+          Company
+        </div>
+        <div className="cell">
+          Quantity
+        </div>
+        <div className="cell">
+          Avg. Price Paid
+        </div>
+      </div>
+
       {portfolio.map(p => {
+        const avgPricePaid = p.pricePaid.reduce((a,b) => a+b, 0)/p.pricePaid.length;
         return (
-          <div>
-            {JSON.stringify(p)}
-          </div>
+          <Link to={`/symbol/${p.symbol}`}className="row" key={p.name}>
+            <div className="cell">
+              {p.name}
+            </div>
+            <div className="cell">
+              {p.quantity}
+            </div>
+            <div className="cell">
+              {moneyFormat(avgPricePaid)}
+            </div>
+          </Link>
+
         )
       })}
     </div>
--- src/containers/PortfolioListContainer/index.js
+++ src/containers/PortfolioListContainer/index.js
@@ -3,11 +3,20 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../../actions';
 import PortfolioList from '../../components/PortfolioList';
+import { moneyFormat } from '../../utils';
+import './index.css';
 
 const PortfolioListContainer = props => {
+  const { cash, stocks } = props.portfolio;
+
   return (
-    <div>
-      <PortfolioList portfolio={props.portfolio.stocks} />
+    <div className="portfolio-list-container">
+      <div className="portfolio-list-container-text">
+        <div>Current Portfolio</div>
+        <div>Cash: {moneyFormat(cash)}</div>
+      </div>
+
+      <PortfolioList portfolio={stocks} />
     </div>
   )
 }
