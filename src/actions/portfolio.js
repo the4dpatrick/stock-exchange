f89ba9cbc3854485d0df23a45413917cd9cc27b8
Patrick Perey <the4dpatrick@yahoo.com>
2016-11-06 11:37:38 -0800

    Buy Sell Stock logic

--- src/actions/portfolio.js
+++ src/actions/portfolio.js
@@ -6,41 +6,115 @@ import {
   SELL_STOCK_SUCCESS,
   SELL_STOCK_ERROR
 } from '../constants';
+import { uniq, find, findIndex } from 'lodash';
 
-export function buyStock(symbol, quantity) {
-  return dispatch => {
+export function buyStock(symbol, name, buyQuantity, bidPrice) {
+  return (dispatch, getState) => {
     dispatch({ type: BUY_STOCK });
-    // TODO: error case - overspending cash (negative balance)
+
+    const { portfolio } = getState();
+    const { stocks: currentStocks, cash: currentCash } = portfolio;
+
+    const newCash = currentCash - (buyQuantity*bidPrice);
+    const hasOverspent = newCash < 0;
+    if (hasOverspent) {
+      return dispatch(buyStockError("Not enough cash for that buy order"));
+    }
+
+    let newStocks;
+    const selectedStock = find(currentStocks, s => s.symbol);
+    const hasStock = selectedStock !== undefined;
+    if (hasStock) {
+      const newQuantity = selectedStock.quantity + buyQuantity;
+      const newPricePaid = uniq([...selectedStock.pricePaid, bidPrice]);
+      const currentStockIndex = findIndex(currentStocks, s => selectedStock);
+      newStocks = [
+        ...currentStocks.slice(0,currentStockIndex),
+        { ...selectedStock, quantity: newQuantity, pricePaid: newPricePaid },
+        ...currentStocks.slice(currentStockIndex+1)
+      ];
+    } else {
+      newStocks = [
+        ...currentStocks,
+        {
+          symbol,
+          name,
+          quantity: buyQuantity,
+          pricePaid: [bidPrice]
+        }
+      ];
+    }
+    dispatch(buyStockSuccess(newCash, newStocks))
   }
 }
 
-export function sellStock(symbol, quantity) {
-  return dispatch => {
+export function sellStock(symbol, sellQuantity, askPrice) {
+  return (dispatch, getState) => {
     dispatch({ type: SELL_STOCK });
-    // TODO: error case - sell more shares than available in portfolio
+
+    const { portfolio } = getState();
+    const { stocks: currentStocks, cash: currentCash } = portfolio;
+
+    const selectedStock = find(currentStocks, s => s.symbol === symbol);
+    const hasStock = selectedStock !== undefined;
+    if (!hasStock) {
+      return dispatch(sellStockError(`You don't own any ${symbol}`));
+    }
+
+    const currentQuantity = selectedStock.quantity;
+    const hasOverSold = sellQuantity > currentQuantity;
+    if (hasOverSold) {
+      return dispatch(sellStockError(`You don't own that many of ${symbol}`))
+    }
+
+    const newCash = currentCash + (sellQuantity*askPrice);
+    const newQuantity = currentQuantity - sellQuantity;
+    let newStocks;
+    if (newQuantity > 0) {
+      const stockIndex = findIndex(currentStocks, s => s.symbol === symbol);
+      newStocks = [
+        ...currentStocks.slice(0, stockIndex),
+        { ...selectedStock, quantity: newQuantity },
+        ...currentStocks.slice(stockIndex+1),
+      ]
+    } else {
+      newStocks = currentStocks.filter(s => s.symbol === symbol);
+    }
+
+    dispatch(sellStockSuccess(newCash, newStocks));
   }
 }
 
-function buyStockSuccess() {
+function buyStockSuccess(cash, stocks) {
   return {
-    type: BUY_STOCK_SUCCESS
+    type: BUY_STOCK_SUCCESS,
+    payload: {
+      cash,
+      stocks
+    }
   }
 }
 
-function buyStockError() {
+function buyStockError(err) {
   return {
-    type: BUY_STOCK_ERROR
+    type: BUY_STOCK_ERROR,
+    payload: err
   }
 }
 
-function sellStockSuccess() {
+function sellStockSuccess(cash, stocks) {
   return {
-    type: SELL_STOCK_SUCCESS
+    type: SELL_STOCK_SUCCESS,
+    payload: {
+      cash,
+      stocks
+    }
   }
 }
 
-function sellStockError() {
+function sellStockError(err) {
   return {
-    type: SELL_STOCK_ERROR
+    type: SELL_STOCK_ERROR,
+    payload: err
   }
 }
