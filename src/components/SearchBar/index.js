e1587379b3da9693b63434e1c5d2dd5d377f1df1
Patrick Perey <the4dpatrick@yahoo.com>
2016-11-06 11:38:35 -0800

    Search Bar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -2,10 +2,19 @@ import React, { Component, PropTypes as P } from 'react';
 import './index.css';
 
 class SearchBar extends Component {
+  search(e) {
+    if (e.key === 'Enter') {
+      this.props.search(e.nativeEvent.target.value);
+    }    
+  }
   render() {
     return (
       <div id="search-bar">
-        <input type="text" placeholder="Search Stock Symbol..." />
+        <input
+          type="text"
+          placeholder="Search Stock Symbol..."
+          onKeyPress={this.search.bind(this)}
+        />
       </div>
     )
   }
