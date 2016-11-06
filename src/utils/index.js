65413ce7063c74af33d36085396891105cbf8308
Patrick Perey <the4dpatrick@yahoo.com>
2016-11-06 11:33:17 -0800

    Add money format util

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,3 +16,13 @@ export function requestSymbol(symbol) {
       .catch(reject)
   });
 }
+
+/*eslint no-cond-assign: "off"*/
+export function moneyFormat(price, sign='$') {
+  const pieces = parseFloat(price).toFixed(2).split('');
+  let ii = pieces.length - 3;
+  while ((ii-=3) > 0) {
+    pieces.splice(ii, 0, ',');
+  }
+  return sign + pieces.join('');
+}
